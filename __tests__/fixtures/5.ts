680b7f96b85d2f9907b3aa5d3ce11bda67b41608
Petter Kaspersen <petter@kaspersen.dev>
2025-01-13 22:13:08 +0100

    More test fixtures

---
+++ __tests__/fixtures/5.ts
@@ -0,0 +1,54 @@
+import { Item } from "../../src/types";
+
+const itemText = `Item Class: Belts
+Rarity: Rare
+Mind Twine
+Ornate Belt
+--------
+Charm Slots: 1
+--------
+Requirements:
+Level: 31
+--------
+Item Level: 52
+--------
+13% reduced Charm Charges used (implicit)
+--------
++77 to maximum Life
++12 to Strength
++25% to Fire Resistance
++15% to Lightning Resistance`;
+
+const expectedResult: Item = {
+  itemClass: "Belts",
+  itemLevel: 52,
+  itemRarity: "Rare",
+  itemName: {
+    lines: ["Mind Twine", "Ornate Belt"],
+    name: "Mind Twine Ornate Belt",
+  },
+  affixes: [
+    "+77 to maximum Life",
+    "+12 to Strength",
+    "+25% to Fire Resistance",
+    "+15% to Lightning Resistance",
+  ],
+  requirements: {
+    level: 31,
+  },
+  charmSlots: 1,
+  sockets: 0,
+  runes: [],
+  implicits: ["13% reduced Charm Charges used"],
+  corrupted: false,
+  stats: {
+    spirit: undefined,
+    armour: undefined,
+    evasionRating: undefined,
+    energyShield: undefined,
+  },
+  enchants: [],
+  identified: true,
+};
+
+export { itemText, expectedResult };
---
+++ __tests__/fixtures/6.ts
@@ -0,0 +1,58 @@
+import { Item } from "../../src/types";
+
+const itemText = `Item Class: Helmets
+Rarity: Unique
+Wings of Caelyn
+Rusted Greathelm
+--------
+Quality: +17% (augmented)
+Armour: 34 (augmented)
+--------
+Item Level: 65
+--------
+11% increased Rarity of Items found
++21 to Dexterity
+Gain 1 Rage on Melee Hit
+Every Rage also grants 1% increased Stun Threshold
+--------
+The older brother retained calm in the midst of fury.
+--------
+Corrupted`;
+
+const expectedResult: Item = {
+  itemClass: "Helmets",
+  itemLevel: 65,
+  itemRarity: "Unique",
+  itemName: {
+    lines: ["Wings of Caelyn", "Rusted Greathelm"],
+    name: "Wings of Caelyn Rusted Greathelm",
+  },
+  affixes: [
+    "11% increased Rarity of Items found",
+    "+21 to Dexterity",
+    "Gain 1 Rage on Melee Hit",
+    "Every Rage also grants 1% increased Stun Threshold",
+  ],
+  requirements: {
+    level: undefined,
+    strength: undefined,
+    intelligence: undefined,
+    dexterity: undefined,
+  },
+  quality: 17,
+  sockets: 0,
+  runes: [],
+  implicits: [],
+  corrupted: true,
+  stats: {
+    spirit: undefined,
+    armour: 34,
+    evasionRating: undefined,
+    energyShield: undefined,
+  },
+  enchants: [],
+  identified: true,
+  flavorText: "The older brother retained calm in the midst of fury."
+};
+
+export { itemText, expectedResult };
