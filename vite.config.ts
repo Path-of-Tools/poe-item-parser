b0ef1acc290c3eb466e0ec454f52f0412f0e14e1
Petter Kaspersen <petter@kaspersen.dev>
2025-01-15 01:21:21 +0100

    Fix build

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,41 @@
-import { defineConfig } from "vite";
-import dts from "vite-plugin-dts";
 import path from "path";
+import { defineConfig } from "vite";
+import packageJson from "./package.json";
+
+const getPackageName = () => {
+  return packageJson.name;
+};
+
+const getPackageNameCamelCase = () => {
+  try {
+    return getPackageName().replace(/-./g, char => char[1].toUpperCase());
+  } catch (err) {
+    throw new Error("Name property in package.json is missing.");
+  }
+};
+
+const fileName = {
+  es: `${getPackageName()}.js`,
+  iife: `${getPackageName()}.iife.js`,
+};
+
+const formats = Object.keys(fileName) as Array<keyof typeof fileName>;
 
 export default defineConfig({
-  plugins: [dts()],
+  base: "./",
   build: {
+    outDir: "./dist",
     lib: {
       entry: path.resolve(__dirname, "src/index.ts"),
-      name: "PoE2ItemParser",
-      fileName: (format) => `poe2-item-parser.${format}.js`,
+      name: getPackageNameCamelCase(),
+      formats,
+      fileName: format => fileName[format],
     },
+  },
+  resolve: {
+    alias: [
+      { find: "@", replacement: path.resolve(__dirname, "src") },
+      { find: "@@", replacement: path.resolve(__dirname) },
+    ],
   },
 });
